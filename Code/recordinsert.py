38f7f81596a060468f1c7ceb883b84479a246d2f
andrewtaylorpa <andy.taylor@pamediagroup.com>
2023-11-01 11:27:30 +0000

    Amended functions to allow for testing

--- Code/recordinsert.py
+++ Code/recordinsert.py
@@ -1,3 +1,4 @@
+from re import search
 import pyodbc
 
 connectionString = r'DRIVER={ODBC Driver 13 for SQL Server};Server=DV00484\SQLEXPRESS;UID=andyt;PWD=dbpassword;'
@@ -13,43 +14,20 @@ for record in insertrows:
 
 """
 
-def recordManual():
+def recordManual(Hostname,AssetTag,Description,DeviceType,CurrentUser,Department,CostCentre,CostCode,PurchaseDate,Status):
     print("Function to manually add data to database")
-    print("Please enter the following information:")
-    Hostname = input("Hostname: ")
-    AssetTag = input("AssetTag: ")
-    Description = input("Description: ")
-    DeviceType = input("Device Type: ")
-    CurrentUser = input("Current User: ")
-    Department = input("Department: ")
-    CostCentre = input("Cost Centre: ")
-    CostCode =  input("Cost Code: ")
-    PurchaseDate = input("Purchase Date: ")
-    Status = input("Status: ")
-       
-    print("The details you entered are:")
-    print(Hostname)
-    print(AssetTag)
-    print(Description)
-    print(DeviceType)
-    print(CurrentUser)
-    print(Department)
-    print(CostCentre)
-    print(CostCode)
-    print(PurchaseDate)
-    print(Status)
     
-    correct = input("Are the above details correct? - Yes/No: ")
-    if correct.lower() == 'yes':
-        print("Inporting record into database")
-        sqlqueryvar = "INSERT INTO AssetRegister (Hostname,AssetTag,Description,DeviceType,CurrentUser,Department,CostCentre,CostCode,PurchaseDate,Status) VALUES ('" + Hostname + "','" + AssetTag + "','" + Description + "','" + DeviceType + "','" + CurrentUser + "','" + Department + "','" + CostCentre + "','" + CostCode + "','" + PurchaseDate + "','" + Status + "')"
-        cur.execute(sqlqueryvar)
-        print("Committing record to database")
-        conn.commit()
+    print("Inporting record into database")
+    sqlqueryvar = ("INSERT INTO AssetRegister (Hostname,AssetTag,Description,DeviceType,CurrentUser,Department,CostCentre,"
+                   "CostCode,PurchaseDate,Status) VALUES ('" + Hostname + "','" + AssetTag + "','" + Description + "',"
+                   "'" + DeviceType + "','" + CurrentUser + "','" + Department + "','" + CostCentre + "','" + CostCode + "',"
+                   "'" + PurchaseDate + "','" + Status + "')")
+    cur.execute(sqlqueryvar)
+    print("Committing record to database")
+    conn.commit()
 
-    else:
-        print("Please re-enter the details")
-        print("Returning to menu")
+    return sqlqueryvar
+    print("Returning to menu")
 
 def csvImport():
     print("")    
@@ -72,7 +50,12 @@ def csvImport():
     index = 0
     
     for Assets in Assetslist:
-        sqlstring = "INSERT INTO AssetRegister (Hostname,AssetTag,Description,DeviceType,CurrentUser,Department,CostCentre,CostCode,PurchaseDate,Status) VALUES ('" + Assetslist[index][0] + "','" + Assetslist[index][1] + "','" + Assetslist[index][2] + "','" + Assetslist[index][3] + "','" + Assetslist[index][4] + "','" + Assetslist[index][5] + "','" + Assetslist[index][6] + "','" + Assetslist[index][7] + "','" + Assetslist[index][8] + "','" + Assetslist[index][9] + "')"
+        sqlstring = ("INSERT INTO AssetRegister (Hostname,AssetTag,Description,DeviceType,CurrentUser,"
+                     "Department,CostCentre,CostCode,PurchaseDate,Status)"
+                     "VALUES ('" + Assetslist[index][0] + "','" + Assetslist[index][1] + "','" + Assetslist[index][2] + "',"
+                     "'" + Assetslist[index][3] + "','" + Assetslist[index][4] + "','" + Assetslist[index][5] + "',"
+                     "'" + Assetslist[index][6] + "','" + Assetslist[index][7] + "','" + Assetslist[index][8] + "',"
+                     "'" + Assetslist[index][9] + "')")
         index = index + 1
         try:
             cur.execute(sqlstring)
@@ -95,30 +78,19 @@ def searchHostname():
     except Exception as e:
         print(e)
 
-def searchAll():
-    print("Hostname")
-    print("AssetTag")
-    print("Description")
-    print("DeviceType")
-    print("CurrentUser")
-    print("Department")
-    print("CostCentre")
-    print("CostCode")
-    print("PurchaseDate")
-    print("Status")    
-    searchQuery = input("Select a query field from above: ")
-    searchValue = input("Enter the value to search: ")
+def searchAll(searchQuery,searchValue):
     sqlquery = "SELECT * FROM AssetRegister WHERE " + searchQuery + "='" + searchValue + "'"
     print("")
     print("SQL Query = " + sqlquery)
     try:
-        result = cur.execute(sqlquery).fetchall()
-        for row in result:
-            print ("")   
-            print(row)
-            print("")
+        results = cur.execute(sqlquery).fetchall()
+        ResultList = []
+        for row in results:
+            print ("")
+            ResultList.append(row)
     except Exception as e:
         print(e)
+    return ResultList
     
 
 def finish():
@@ -140,11 +112,55 @@ while selection != 0:
     selection = int(input("Please choose a process: "))
         
     if selection == 1:
-        recordManual()
+        print("Function to manually add data to database")
+        print("Please enter the following information:")
+        Hostname = input("Hostname: ")
+        AssetTag = input("AssetTag: ")
+        Description = input("Description: ")
+        DeviceType = input("Device Type: ")
+        CurrentUser = input("Current User: ")
+        Department = input("Department: ")
+        CostCentre = input("Cost Centre: ")
+        CostCode =  input("Cost Code: ")
+        PurchaseDate = input("Purchase Date: ")
+        Status = input("Status: ")
+       
+        print("The details you entered are:")
+        print(Hostname)
+        print(AssetTag)
+        print(Description)
+        print(DeviceType)
+        print(CurrentUser)
+        print(Department)
+        print(CostCentre)
+        print(CostCode)
+        print(PurchaseDate)
+        print(Status)
+    
+        correct = input("Are the above details correct? - Yes/No: ")
+        if correct.lower() == 'yes':
+            recordManual(Hostname,AssetTag,Description,DeviceType,CurrentUser,Department,CostCentre,CostCode,PurchaseDate,Status)
+        else:
+            print("Returning to menu, please enter the correct details")
+            
     if selection == 2:
         csvImport()
     if selection == 3:
-        searchAll()    
+        print("Hostname")
+        print("AssetTag")
+        print("Description")
+        print("DeviceType")
+        print("CurrentUser")
+        print("Department")
+        print("CostCentre")
+        print("CostCode")
+        print("PurchaseDate")
+        print("Status")    
+        searchQuery = input("Select a query field from above: ")
+        searchValue = input("Enter the value to search: ")
+        mySearchResults = searchAll(searchQuery,searchValue)
+        print(mySearchResults)
+        print("")
     if selection == 0:
         finish()
 
